ca7304e5eb244b77669a9246de4b93f03364cae3
Noah Sluss <slussnoah@gmail.com>
2015-03-30 14:16:53 -0700

    create chapter_one. need to add treeReduce, and unclude underscore in index.

---
+++ Chapter_One/Rats.js
@@ -0,0 +1,19 @@
+// Welcome to the peaceful town of Hamelin :)
+
+var Hamelin = {
+  neighborhoods: [
+    {streets: [
+      {houses: [
+        {rooms: }
+      ]}
+    ]},
+    {streets: []},
+    {streets: []},
+    {streets: []}
+  ],
+
+};
+
+// The peaceful town of Hamelin has been overridden by rats!
+function
+// As you can see, there are a lot of them
